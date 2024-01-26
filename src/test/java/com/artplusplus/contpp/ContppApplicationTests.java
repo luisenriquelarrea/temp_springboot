c03bfeacab5ca99c0ed3470c8f0f5990d5538b02
Luis Enrique Larrea <enrique.larrea@alumno.udg.mx>
2024-01-25 21:57:02 -0600

    INIT contpp spring

---
+++ src/test/java/com/artplusplus/contpp/ContppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artplusplus.contpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

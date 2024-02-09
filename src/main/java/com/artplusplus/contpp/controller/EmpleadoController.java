be5747a71ff8974f817f724720668c72571badba
artplusplusmx <enrique.larrea@alumno.udg.mx>
2024-02-09 12:45:41 -0600

    RFX

--- src/main/java/com/artplusplus/contpp/controller/EmpleadoController.java
+++ src/main/java/com/artplusplus/contpp/controller/EmpleadoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +34,13 @@ public class EmpleadoController {
     public @ResponseBody List<Empleado> all() {
         // This returns a JSON or XML with the users
         return empleadoService.listEmpleado();
+    }
+
+    @PutMapping(path="/{id}")
+    public ResponseEntity<Empleado> update(@RequestBody Empleado empleado,
+                     @PathVariable Long id){
+        Empleado updEmpleado = empleadoService.updateEmpleado(empleado, id);
+        return ResponseEntity.ok(updEmpleado);
     }
 
     @DeleteMapping(path="/{id}")

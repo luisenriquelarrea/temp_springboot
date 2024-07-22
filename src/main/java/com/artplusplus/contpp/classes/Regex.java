08dfdb617ee0110bf0ff1764a56bfb879b500424
luisenriquelarreamx <luis.larrea@montes.com.mx>
2024-07-22 11:07:07 -0600

    RFX

---
+++ src/main/java/com/artplusplus/contpp/classes/Regex.java
@@ -0,0 +1,15 @@
+package com.artplusplus.contpp.classes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    private final static String PATTERN_PHONE_NUMBER = 
+        "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+
+    public static boolean isPhoneNumber(String phoneNumber){
+        Pattern pattern = Pattern.compile(PATTERN_PHONE_NUMBER);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+}

de3c9ce5938b5a43c016c844584fc5bca131e335
luisenriquelarreamx <luis.larrea@montes.com.mx>
2024-09-18 13:38:21 -0600

    RFX

--- src/main/java/com/artplusplus/contpp/utils/Funciones.java
+++ src/main/java/com/artplusplus/contpp/utils/Funciones.java
@@ -5,13 +5,43 @@ import java.io.ByteArrayOutputStream;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.export.OutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 
 public class Funciones {
     public static byte[] getPDFJasperReport(JasperPrint jasperPrint) throws JRException{
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         JasperExportManager.exportReportToPdfStream(jasperPrint, baos);
-        byte[] pdfBytes = baos.toByteArray();
-        return pdfBytes;
+        byte[] bytes = baos.toByteArray();
+        return bytes;
+    }
+
+    public static byte[] getXLSJasperReport(JasperPrint jasperPrint) throws JRException{
+        SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
+        configuration.setOnePagePerSheet(true);
+        configuration.setDetectCellType(true);
+        configuration.setWhitePageBackground(false);
+        configuration.setFontSizeFixEnabled(false);
+        
+        // No spaces between rows and columns 
+        configuration.setRemoveEmptySpaceBetweenRows(true); 
+        configuration.setRemoveEmptySpaceBetweenColumns(true);
+        
+        // If you want to name sheets then uncomment this line
+        // configuration.setSheetNames(new String[] { "Data" });
+        final JRXlsExporter exporter = new JRXlsExporter();
+        exporter.setConfiguration(configuration);
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(baos);
+        exporter.setExporterOutput(exporterOutput);
+        exporter.exportReport();
+        byte[] bytes = baos.toByteArray();
+        return bytes;
     }
 
     public static String toTimestamp(String fecha){

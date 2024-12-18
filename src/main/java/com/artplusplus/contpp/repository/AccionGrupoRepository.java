cafcacb2e2b8c74204e406c1d29701a89f349dce
luisenriquelarreamx <luis.larrea@montes.com.mx>
2024-12-17 22:20:21 -0600

    RFX

--- src/main/java/com/artplusplus/contpp/repository/AccionGrupoRepository.java
+++ src/main/java/com/artplusplus/contpp/repository/AccionGrupoRepository.java
@@ -1,12 +1,58 @@
 package com.artplusplus.contpp.repository;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.artplusplus.contpp.model.Accion;
 import com.artplusplus.contpp.model.AccionGrupo;
+import com.artplusplus.contpp.model.SeccionMenu;
 
-import com.artplusplus.contpp.repository.customMethods.AccionGrupoCustomMethods;
+import java.util.List;
 
-public interface AccionGrupoRepository extends JpaRepository<AccionGrupo, Long>, AccionGrupoCustomMethods, JpaSpecificationExecutor<AccionGrupo>{
-    
+public interface AccionGrupoRepository extends JpaRepository<AccionGrupo, Long>, JpaSpecificationExecutor<AccionGrupo>{
+    @Query("SELECT ag.accion "+
+        "FROM AccionGrupo ag "+
+        "WHERE ag.grupo.id = :grupoId "+
+        "AND ag.accion.seccionMenu.id = :seccionMenuId "+
+        "AND ag.status = 1 "+
+        "AND ag.grupo.status = 1 "+
+        "AND ag.accion.status = 1 "+
+        "AND ag.accion.onBreadcrumb = 1 ")
+    List<Accion> getAllowedBreadcrumbs(@Param("grupoId") Long grupoId, 
+        @Param("seccionMenuId") Long seccionMenuId);
+
+        @Query("SELECT ag.accion "+
+        "FROM AccionGrupo ag "+
+        "WHERE ag.grupo.id = :grupoId "+
+        "AND ag.accion.seccionMenu.id = :seccionMenuId "+
+        "AND ag.status = 1 "+
+        "AND ag.grupo.status = 1 "+
+        "AND ag.accion.status = 1 "+
+        "AND ag.accion.onNavbar = 1 ")
+    List<Accion> getAllowedNavbar(@Param("grupoId") Long grupoId, 
+        @Param("seccionMenuId") Long seccionMenuId);
+
+    @Query("SELECT ag.accion.seccionMenu "+
+        "FROM AccionGrupo ag "+
+        "WHERE ag.grupo.id = :grupoId "+
+        "AND ag.status = 1 "+
+        "AND ag.grupo.status = 1 "+
+        "AND ag.accion.status = 1 "+
+        "AND ag.accion.onBreadcrumb = 1 "+
+        "AND ag.accion.seccionMenu.status = 1 "+
+        "AND ag.accion.seccionMenu.menu.status = 1 ")
+    List<SeccionMenu> getAllowedMenus(@Param("grupoId") Long grupoId);
+
+    @Query("SELECT ag.accion "+
+        "FROM AccionGrupo ag "+
+        "WHERE ag.grupo.id = :grupoId "+
+        "AND ag.accion.seccionMenu.id = :seccionMenuId "+
+        "AND ag.status = 1 "+
+        "AND ag.grupo.status = 1 "+
+        "AND ag.accion.status = 1 "+
+        "AND ag.accion.onTable = 1 ")
+    List<Accion> getAllowedTableActions(@Param("grupoId") Long grupoId, 
+        @Param("seccionMenuId") Long seccionMenuId);
 }
